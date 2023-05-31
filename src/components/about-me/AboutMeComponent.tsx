3dbe9d7eb24b250b14b548d0ded5780799230cb3
FerRivera <FerRivera2508@gmail.com>
2023-05-31 18:12:22 +0100

    About me ready

---
+++ src/components/about-me/AboutMeComponent.tsx
@@ -0,0 +1,38 @@
+import { useTranslation } from 'react-i18next';
+
+export const AboutMeComponent = () => {
+  const { t } = useTranslation();
+
+  return (
+    <section className="bg-white dark:bg-gray-900">
+      <div className="relative flex">
+        <div className="min-h-screen lg:w-1/3"></div>
+        <div className="hidden w-3/4 min-h-screen bg-gray-100 dark:bg-gray-800 lg:block"></div>
+
+        <div className="container flex flex-col justify-center w-full min-h-screen px-6 py-10 mx-auto lg:absolute lg:inset-x-0">
+          <h1 className="text-2xl font-semibold text-gray-800 capitalize lg:text-3xl dark:text-white">
+            Fernando <span className="text-blue-800">Rivera</span> <br />
+          </h1>
+
+          <div className="mt-10 lg:mt-20 lg:flex lg:items-center">
+            <img
+              className="object-cover object-center w-full lg:w-[32rem] rounded-lg h-96"
+              src="https://res.cloudinary.com/fercloudinary/image/upload/v1685546396/FerRivera%20Web/mainPhoto_gspdi0.jpg"
+              alt=""
+            />
+
+            <div className="mt-8 lg:px-10 lg:mt-0">
+              <h1 className="text-2xl font-semibold text-gray-800 dark:text-white lg:w-72">
+                {t('aboutMe.aboutMe')}
+              </h1>
+
+              <p className="max-w-lg mt-6 text-gray-500 dark:text-gray-400">
+                {t('aboutMe.description')}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
--- src/screens/AboutMe.tsx
+++ src/screens/AboutMe.tsx
@@ -1,9 +1,15 @@
 import { useEffect } from 'react';
 
+import { AboutMeComponent } from '../components/about-me/AboutMeComponent';
+
 export const AboutMe = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
-  return <div>AboutMe</div>;
+  return (
+    <div className="px-4 pt-15 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:pt-20">
+      <AboutMeComponent />
+    </div>
+  );
 };
